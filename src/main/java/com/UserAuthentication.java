2ff775271b3e26d34f49718564d3a06baa88402f
Saman Shrestha <samanshrestha1000@gmail.com>
2022-08-30 15:16:15 +0530

    firstcommit

---
+++ src/main/java/com/UserAuthentication.java
@@ -0,0 +1,16 @@
+package com;
+
+public class UserAuthentication {
+	public String getUserName() {
+		return "saman";
+	}
+	public String getEmail() {
+		return "saman@gmail.com";
+	}
+	public int getPhnNum() {
+		return 123456789;
+	}
+	public String getPassword() {
+		return "saman@123";
+	}
+}
